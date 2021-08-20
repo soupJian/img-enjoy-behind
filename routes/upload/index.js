4f06e1ee5da4661760a428cf921d2e56c208b4b7
soupJian <soupjian@163.com>
2021-08-20 10:59:10 +0800

    feat:上传图片获取图片宽高比例

--- routes/upload/index.js
+++ routes/upload/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var multer=require('multer');
 var fs=require('fs');
+const sizeOf = require('image-size')
 const {mysqlRequest} = require('../../util/mysql')
 const {successCode,defaultDescription,admin} = require('../../util/type')
 
@@ -20,24 +21,28 @@ router.post('/', upload.array('file',9),async function(req, res, next) {
     const ext = item.originalname.substr(index);
     const oldpath = item.destination + '/' + item.filename
     const newPath = `${item.destination}/${item.filename}${ext}`
-    fs.rename(oldpath,newPath,()=>{})
-    const uploadAddress = `http://175.24.116.96:3300/upload/${item.filename}${ext}`
-    address.push(uploadAddress)
-    let sql
-    if(id === 'null'){
-      sql = `insert into upload values (${admin.id},${time},'${uploadAddress}','${defaultDescription}',0,0)`
-    }else{
-      sql = `insert into upload values (${id},${time},'${uploadAddress}','${defaultDescription}',0,0)`
-    }
-    mysqlRequest(sql).then(()=>{
-      count++
-      if(count === files.length){
-        res.send({
-          code: successCode,
-          message: '上传成功',
-          address
+    fs.rename(oldpath,newPath,()=>{
+      sizeOf(newPath, function (err, dimensions) {
+        const proportion = dimensions.width / dimensions.height
+        const uploadAddress = `http://175.24.116.96:3300/upload/${item.filename}${ext}`
+        address.push(uploadAddress)
+        let sql
+        if(id === 'null'){
+          sql = `insert into upload values (${admin.id},${time},'${uploadAddress}','${proportion}','${defaultDescription}',0,0)`
+        }else{
+          sql = `insert into upload values (${id},${time},'${uploadAddress}','${proportion}','${defaultDescription}',0,0)`
+        }
+        mysqlRequest(sql).then(()=>{
+          count++
+          if(count === files.length){
+            res.send({
+              code: successCode,
+              message: '上传成功',
+              address
+            })
+          }
         })
-      }
+      })
     })
   });
 });
